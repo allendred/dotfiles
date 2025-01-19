4521ceff4634243b0d724125012c54498774be64
allendred <allendred@163.com>
2025-01-19 14:49:17 +0800

    Refactor zsh config, update plugin paths, and enable autosuggestions

--- zsh/install_plugins.sh
+++ zsh/install_plugins.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
+###
+ # @Author: allendred allendred@163.com
+ # @Date: 2025-01-13 20:02:38
+ # @LastEditors: allendred allendred@163.com
+ # @LastEditTime: 2025-01-19 14:24:59
+ # @FilePath: /.dotfiles/zsh/install_plugins.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 
 # 在 .gitignore 中添加 `zsh/plugins/` 将 zsh 插件全部不 track
 echo "🔄 安装 zsh 插件..."
 
-mkdir -p ~/.zsh/plugins/
+# mkdir -p ~/.oh-my-zsh/custom/plugins/
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/plugins/powerlevel10k
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/plugins/powerlevel10k
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-git clone --depth=1 https://github.com/junegunn/fzf-git.sh.git ~/.zsh/plugins/fzf-git.sh
+git clone --depth=1 https://github.com/junegunn/fzf-git.sh.git ~/.oh-my-zsh/custom/plugins/fzf-git.sh
 
 echo "✅ zsh 插件安装完成"
 echo
--- zshrc
+++ zshrc
@@ -70,10 +70,11 @@ ZSH_THEME="robbyrussell"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git
+    zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.                                                   
 # Initialization code that may require console input (password prompts, [y/n]                                                      
 # confirmations, etc.) must go above this block; everything else may go below.                                                     
